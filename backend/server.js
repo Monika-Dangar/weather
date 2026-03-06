b902ba066c12e59a4c01106fc982496a9d63a105
Monika-Dangar <dangarmonika191023@gmail.com>
2026-03-06 12:11:54 +0530

    cors issues fix

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors({ origin: 'https://weather-frontend-wine.vercel.app' }));
+
 
 // ================= CACHE =================
 
